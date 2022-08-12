952ed0e7d45b88212d04944ba169c190f773b2c1
Kyle Miller <millerkyle85@gmail.com>
2022-08-12 13:14:10 -0700

    using the Parent Component's Method in the Child Component

--- src/components/NewTeaForm.js
+++ src/components/NewTeaForm.js
@@ -2,15 +2,24 @@ import React from "react";
 import { v4 } from 'uuid';
 import PropTypes from "prop-types";
 
-function NewTicketForm(props){
+function NewTeaForm(props){
   function handleNewTeaFormSubmission(event) {
     event.preventDefault();
-  return (
-    <React.Fragment>
-      <h3>This is a form.</h3>
-    </React.Fragment>
-    );
+    props.onNewTeaCreation({
+      name: event.target.name.value,
+      origin: event.target.origin.value,
+      type: event.target.type.value,
+      price: parseInt(event.target.price.value),
+      quantity: parseInt(130),
+      id: v4()
+    });
   }
+  
+    return (
+      <React.Fragment>
+        <h3>This is a form.</h3>
+      </React.Fragment>
+    );
 }
 NewTeaForm.propTypes = {
   onNewTeaCreation: PropTypes.func
