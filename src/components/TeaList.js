b4e15e95b16fc8dc4fad21cf8c2be8342ea50fab
Kyle Miller <millerkyle85@gmail.com>
2022-08-12 12:11:55 -0700

    add tea props

---
+++ src/components/TeaList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Tea from "./Tea";
+
+function TeaList(){
+  return (
+    <React.Fragment>
+      <Tea
+        name="3A"
+        origin="Thato and Haley"
+        type=""
+        price="Firebase will not save record!"
+        quantity=""
+        id=""
+        key=""
+      />
+    </React.Fragment>
+  );
+}
+
+export default TeaList;
