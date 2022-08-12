7eb1851dc2c97545837cf47fbd47001b78c3e925
Kyle Miller <millerkyle85@gmail.com>
2022-08-12 12:29:36 -0700

    update TeaList

--- src/components/TeaList.js
+++ src/components/TeaList.js
@@ -1,18 +1,51 @@
 import React from "react";
 import Tea from "./Tea";
 
+const mainTeaList = [
+  {
+    name:"Green Tea",
+    origin:"China",
+    type:"Matcha",
+    price:10000,
+    quantity:"",
+    id:"",
+    key:""
+  },
+  {
+    name:"Black Tea",
+    origin:"China",
+    type:"Earl Gray",
+    price:1000,
+    quantity:"",
+    id:"",
+    key:""
+  },
+  {
+    name:"Oolong Tea",
+    origin:"China",
+    type:"Tieguanyin",
+    price:1000,
+    quantity:"",
+    id:"",
+    key:""
+  }
+];
+
+
+
 function TeaList(){
   return (
     <React.Fragment>
-      <Tea
-        name="3A"
-        origin="Thato and Haley"
-        type=""
-        price="Firebase will not save record!"
-        quantity=""
-        id=""
-        key=""
-      />
+      <hr/>
+        {mainTeaList.map((tea, index) =>
+          <Tea name={tea.name}
+            origin={tea.origin}
+            type={tea.type}
+            price={tea.price}
+            quantity={tea.quantity}
+            id={tea.id}
+            key={index}/>
+        )}
     </React.Fragment>
   );
 }
