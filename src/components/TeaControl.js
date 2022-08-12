2dcbfd388454cfd5fe81fa60ce3af7d8cd983bad
Kyle Miller <millerkyle85@gmail.com>
2022-08-12 12:40:19 -0700

    add calling a function in a state component

---
+++ src/components/TeaControl.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import NewTeaForm from './NewTeaForm';
+import TeaList from './TeaList';
+
+class TeaControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+  
+  handleClick = () => {
+    this.setState({formVisibleOnPage: true});
+  }
+
+  render(){
+    let currentlyVisibleState = null;
+    let addTeaButton = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTeaForm />
+    } else {
+      currentlyVisibleState = <TeaList />
+      addTeaButton = <button onClick={this.handleClick}>Add Tea</button> 
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        {addTeaButton}
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default TeaControl;
