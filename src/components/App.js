fba46fd33d004b9692937b2f69b1d8701f9cb72f
Kyle Miller <millerkyle85@gmail.com>
2022-08-13 12:59:34 -0700

    revise app.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import TeaControl from "./TeaControl";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <TeaControl />
+
+    </React.Fragment>
+  );
+}
+
+export default App;
