e0262c8b408c285793d4b75386310e8be96e5b6f
Kyle Miller <millerkyle85@gmail.com>
2022-08-13 22:57:49 -0700

    add background img

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Header from "./Header";
 import TeaControl from "./TeaControl";
+import background from "./../img/tea.jpg";
+
 
 function App() {
   return (
-    <React.Fragment>
-      <Header />
-      <TeaControl />
+    <React.Fragment>    
+       <div style={{ backgroundImage:`url(${background})`,
+                    backgroundPosition: "center",
+                    backgroundSize:"cover",
+                    backgroundRepeat:"no-repeat", 
+                    width: "1000px",
+                    height: "1000px"
+                   
+                     }}>
+        
+        <Header />
+        <TeaControl />
+        </div>
+      
 
     </React.Fragment>
   );
