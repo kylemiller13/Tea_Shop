5a7ba2312e0a463fe6911c96e6634c1cde5c64c0
Kyle Miller <millerkyle85@gmail.com>
2022-08-20 20:43:58 -0700

    center name

--- src/components/Tea.js
+++ src/components/Tea.js
@@ -4,17 +4,19 @@ import PropTypes from "prop-types";
 function Tea(props){
   return (
     <React.Fragment>
-      <div onClick={() => props.whenTeaClicked(props.id)}>
-        <h3>Tea Name: {props.name}</h3>
-      </div>
+        <div onClick={() => props.whenTeaClicked(props.id)}>
+          <div className="centerName">
+            <h3>Tea Name: {props.name}</h3>
+          </div>
+        </div>
     </React.Fragment>
   );
 }
 
+
 Tea.propTypes = {
   name: PropTypes.string,
   origin: PropTypes.string,
-  type: PropTypes.string,
   price: PropTypes.number,
   quantity: PropTypes.number,
   id: PropTypes.string,
