6701c23abc3f0c54692d4d92660da9ea53f155fd
Kyle Miller <millerkyle85@gmail.com>
2022-08-20 20:44:40 -0700

    center submit BTN

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,17 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Select from 'react-select';
 
+
+
+const teaNames = [
+  { value: "Macha Tea", label: "Macha Tea", },
+  { value: "Black Tea", label: "Black Tea" },
+  { value: "Oolong Tea", label: "Oolong Tea" },
+  { value: "Rosehip Tea", label: "Rosehip Tea" },
+  { value: "Chai Tea", label: "Chai Tea" }
+];
 
 
 function ReusableForm(props) {
+  
   return (
     <React.Fragment>
       <form onSubmit={props.formSubmissionHandler}>
-        <input type="text" name="name" placeholder="Green Tea" />
-        <input type="text" name="origin" placeholder="China" />
-        <input type="text" name="type" placeholder="Macha" />
-        <input type="number" name="price" placeholder="Price" />
+      <Select name="name"
+          value={teaNames.value}
+          options={teaNames}
+        />
+        <div className="submitBTN">
         <button type="submit">{props.buttonText}</button>
+        </div>
       </form>
     </React.Fragment>
   );
@@ -19,7 +32,7 @@ function ReusableForm(props) {
 
 ReusableForm.propTypes = {
   formSubmissionHandler: PropTypes.func,
-  buttonText: PropTypes.string,
+  buttonText: PropTypes.string
 };
 
 export default ReusableForm;
