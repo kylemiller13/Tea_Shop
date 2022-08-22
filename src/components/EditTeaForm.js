424279d3477ee02860dde24dfde29603162dcd80
Kyle Miller <millerkyle85@gmail.com>
2022-08-22 16:27:16 -0700

    commented  out

--- src/components/EditTeaForm.js
+++ src/components/EditTeaForm.js
@@ -1,33 +1,32 @@
-import React from "react";
-import ReusableForm from "./ReusableForm";
-import PropTypes from "prop-types";
+// import React from "react";
+// import ReusableForm from "./ReusableForm";
+// import PropTypes from "prop-types";
 
-function EditTeaForm(props) {
-  const { tea } = props;
+// function EditTeaForm(props) {
+//   const { tea } = props;
 
-  function handledEditTeaFormSubmission(event) {
-    event.preventDefault();
-    props.onEditTea({
-      name: event.target.name.value,
-      origin: event.target.origin.value,
-      type: event.target.type.value,
-      price: parseInt(event.target.price.value),
-      id: tea.id,
-    });
-  }
-  return (
-    <React.Fragment>
-      <ReusableForm
-        formSubmissionHandler={handledEditTeaFormSubmission}
-        buttonText="Update Tea"
-      />
-    </React.Fragment>
-  );
-}
+//   function handledEditTeaFormSubmission(event) {
+//     event.preventDefault();
+//     props.onEditTea({
+//       name: event.target.name.value,
+//       origin: "China",
+//       price: 130,
+//       id: tea.id,
+//     });
+//   }
+//   return (
+//     <React.Fragment>
+//       <ReusableForm
+//         formSubmissionHandler={handledEditTeaFormSubmission}
+//         buttonText="Update Tea"
+//       />
+//     </React.Fragment>
+//   );
+// }
 
-EditTeaForm.propTypes = {
-  tea: PropTypes.object,
-  onEditTea: PropTypes.func,
-};
+// EditTeaForm.propTypes = {
+//   tea: PropTypes.object,
+//   onEditTea: PropTypes.func,
+// };
 
-export default EditTeaForm;
+// export default EditTeaForm;
